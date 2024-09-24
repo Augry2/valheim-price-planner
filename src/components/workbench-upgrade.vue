0e091fe7514747908e1218f8194d445e8be5cbdf
august <august.ryd@gmail.com>
2024-09-24 15:57:06 +0200

    printing out locations and items for workbench

--- src/components/workbench-upgrade.vue
+++ src/components/workbench-upgrade.vue
@@ -3,50 +3,77 @@ import { ref, computed } from 'vue';
 
 // Component data
 const data = ref({
-    meadows: {
-        choppingBlock: {
-            wood: 10,
-            flint: 10
+    // Adding locations array under data
+    locations: [
+        {
+            name: "Meadows",
+            items: {
+                choppingBlock: {
+                    name: "Chopping Block",
+                    wood: 10,
+                    flint: 10
+                },
+                tanningRack: {
+                    name: "Tanning Rack",
+                    wood: 10,
+                    flint: 15,
+                    leatherScraps: 20,
+                    deerHide: 5
+                }
+            },
+            showItems: false,
         },
-        tanningRack: {
-            wood: 10,
-            flint: 15,
-            leatherScraps: 20,
-            deerHide: 5
+        {
+            name: "Dark Forest",
+            items: {
+                adze: {
+                    name: "Adze",
+                    fineWood: 10,
+                    bronze: 3
+                }
+            },
+            showItems: false,
+        },
+        {
+            name: "Mountains",
+            items: {
+                toolShelf: {
+                    name: "Tool Shelf",
+                    fineWood: 10,
+                    iron: 4,
+                    obsidian: 4
+                }
+            },
+            showItems: false,
         }
-    },
-
-    darkForest: {
-        adze: {
-            fineWood: 10,
-            bronze: 3
-        }
-    },
-
-    mountains: {
-        toolShelf: {
-            fineWood: 10,
-            iron: 4,
-            obsidian: 4
-        }
-    }
-
+    ]
 });
 
-// Computed properties
-const computedProperties = computed(() => {
-    // Your computed properties logic goes here
-});
 
 // Methods
-const methods = {
-    // Your component methods go here
+const toggleItems = (location) => {
+    location.showItems = !location.showItems; // Toggle showItems on click
+    console.log(location.showItems);
 };
+
 </script>
 
 <template>
     <div class="workbench-upgrade">
-        <a>workbench-upgrade</a>
+        <h2>Workbench Upgrades</h2>
+
+        <ul>
+            <li v-for="location in data.locations" :key="location.name">
+                <h3>{{ location.name }} <button @click="toggleItems(location)">+</button></h3>
+                <ul v-if="location.showItems">
+                    <li v-for="item in location.items" :key="item.name">
+                        {{ item.name }}
+                    </li>
+
+                </ul>
+            </li>
+        </ul>
+
     </div>
 </template>
 
